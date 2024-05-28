6fbeab1117f16baecba48f5f87a2a1638c7f1da8
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-28 14:38:16 +0300

    upd

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/api.js';
+import './js/header/header.js';
+import './js/hero/hero.js';
+import './js/mobile-menu/mobile-menu.js';
+import './js/about-me/about-me.js';
+import './js/stack/stack.js';
+import './js/benefits/benefits.js';
+import './js/projects/projects.js';
+import './js/faq/faq.js';
+import './js/cover/cover.js';
+import './js/reviews/reviews.js';
+import './js/footer/footer.js';
