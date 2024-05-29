39582f8edcacc6eaa9a80c2b8cb6b035d97e8d4c
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-29 10:12:34 +0300

    upd

---
+++ src/js/projects/projects.js
@@ -0,0 +1,13 @@
+import imgBookDesk from '../../img/projects/desktop/book-desk@1x.png';
+import imgBookDeskRetina from '../../img/projects/desktop/book-desk@2x.png';
+
+const projects = [
+  {
+    title: 'Programming Across Borders: Ideas, Technologies, Innovations',
+  },
+  {},
+  {},
+];
+
+console.log(projects);
+function cardTemplate(obj) {}
