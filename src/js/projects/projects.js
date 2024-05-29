8dcde7f941682ca5f6e5f378136c0bae477c7af5
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-29 11:54:58 +0300

    upd

--- src/js/projects/projects.js
+++ src/js/projects/projects.js
@@ -1,13 +1,31 @@
 import imgBookDesk from '../../img/projects/desktop/book-desk@1x.png';
 import imgBookDeskRetina from '../../img/projects/desktop/book-desk@2x.png';
+import imgBookTab from '../../img/projects/tablet/book-tab@1x.png';
+import imgBookTabRetina from '../../img/projects/tablet/book-tab@2x.png';
+import imgBookMob from '../../img/projects/mobile/book-mob@1x.png';
+import imgBookMobRetina from '../../img/projects/mobile/book-mob@2x.png';
 
 const projects = [
   {
     title: 'Programming Across Borders: Ideas, Technologies, Innovations',
+    imgLinkDesk: imgBookDesk,
+    imgLinkDeskRetina: imgBookDeskRetina,
+    imgLinkTab: imgBookTab,
+    imgLinkTabRetina: imgBookTabRetina,
+    imgLinkMob: imgBookMob,
+    imgLinkMobRetina: imgBookMobRetina,
   },
   {},
   {},
 ];
 
+const ref = {
+  projectSectionContainer: document.querySelector('.projects-js'),
+};
+
+console.log();
+
 console.log(projects);
-function cardTemplate(obj) {}
+function cardTemplate(obj) {
+  return ``;
+}
