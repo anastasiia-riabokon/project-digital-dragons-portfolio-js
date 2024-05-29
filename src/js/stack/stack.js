c8a827e1bdc3034ff74db319d439ed4f1d3d260b
lukinivan <Lukinivan09@gmail.com>
2024-05-29 19:10:59 +0100

    next comit

--- src/js/stack/stack.js
+++ src/js/stack/stack.js
@@ -1,35 +1,33 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
 
-const stackList = document.querySelector('.swiper-button');
+const stackList = document.querySelector('.stack-list');
+const nextBtn = document.querySelector('.js-next-button');
 
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 3,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: '.swiper-button-next',
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    375: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    // when window width is >= 480px
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    1440: {
-      slidesPerView: 6,
-      spaceBetween: 40,
-    },
-  },
-});
+// const swiper = new Swiper('.swiper', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//   },
+//   breakpoints: {
+//     375: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//     1440: {
+//       slidesPerView: 6,
+//       spaceBetween: 40,
+//     },
+//   },
+// });
 
-stackList.addEventListener('click', e => {
-  swiper.slideNext(300, false);
-});
+// stackList.addEventListener('click', e => {
+//   swiper.slideNext(300, false);
+// });
