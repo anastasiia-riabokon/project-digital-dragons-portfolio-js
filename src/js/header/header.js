2de399fba004874187bcf70b10f0eac30d1509b8
LieraBerh <lierrastern@gmail.com>
2024-05-30 12:21:27 +0200

    upd

---
+++ src/js/header/header.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu-list');
+
+menuList.style.display = 'none';
+
+menuBtn.addEventListener('click', () => {
+  if (menuList.style.display === 'none') {
+    menuList.style.display = 'block';
+  } else {
+    menuList.style.display = 'none';
+  }
+});
