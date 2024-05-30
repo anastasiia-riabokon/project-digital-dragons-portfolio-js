a76205ace09c9f177268e1701f2752f8a5d1faef
lukinivan <Lukinivan09@gmail.com>
2024-05-30 08:47:37 +0100

    styles

--- src/js/stack/stack.js
+++ src/js/stack/stack.js
@@ -1,33 +1,28 @@
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 const stackList = document.querySelector('.stack-list');
-const nextBtn = document.querySelector('.js-next-button');
+const nextBtn = document.querySelector('.js-arrow-wrap');
 
-// const swiper = new Swiper('.swiper', {
-//   direction: 'horizontal',
-//   loop: true,
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//   },
-//   breakpoints: {
-//     375: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     768: {
-//       slidesPerView: 3,
-//       spaceBetween: 30,
-//     },
-//     1440: {
-//       slidesPerView: 6,
-//       spaceBetween: 40,
-//     },
-//   },
-// });
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+});
 
-// stackList.addEventListener('click', e => {
-//   swiper.slideNext(300, false);
-// });
+nextBtn.addEventListener('click', e => {
+  swiper.slideNext(300, false);
+});
