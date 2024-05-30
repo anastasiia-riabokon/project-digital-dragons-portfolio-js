20f39d91824334ae4e6c7b0d09690120db2c5a7b
defaultnastia <anastayaaa@gmail.com>
2024-05-30 19:26:11 +0200

    fixed gaps

--- src/js/about-me/about-me.js
+++ src/js/about-me/about-me.js
@@ -6,7 +6,6 @@ const aboutMeArray = [
     title: 'About me',
     content: [
       'I am Lloyd Jefferson, a talented programmer with extensive expertise in software development. I have an understanding of different programming languages and technologies, always ready to improve my skills. I always follow the latest trends and look for unconventional, creative solutions to problems. I have a high level of analytical skills and am able to effectively solve even the most difficult tasks encountered on the way.',
-      '',
       'Able to work both independently and in a team. I can effectively cooperate with colleagues, exchanging ideas and finding optimal solutions. Professional maturity allows you to calmly cope with challenges and stressful situations, while maintaining a high quality of work. I am always looking for opportunities for self-improvement. I actively study new technologies and practices to stay abreast of the latest innovations. I have a strong understanding of various programming languages, frameworks and architectural concepts that allow me to create efficient and scalable software.',
     ],
   },
@@ -37,11 +36,11 @@ function renderAboutSection(array) {
 }
 
 const arrowMarkup =
-  '<button class="arrow-button"><svg class="arrow-icon js-down" width="20" height="20"><use href="./img/icons.svg#arrow-down"></use></svg></button>';
+  '<button class="arrow-button"><svg class="arrow-icon js-down" width="20" height="20"><use href="./img/icons.svg#arrow"></use></svg></button>';
 
 function createAboutParagraph({ content }) {
-  const text = content.join('<br />');
-  const paragraph = `<div class="ac-panel about-list-text-box"><p class="ac-text">${text}</p></div>`;
+  const text = content.join('</p><p>');
+  const paragraph = `<div class="ac-panel about-list-text-box"><div class="ac-text"><p>${text}</p></div></div>`;
   return paragraph;
 }
 
