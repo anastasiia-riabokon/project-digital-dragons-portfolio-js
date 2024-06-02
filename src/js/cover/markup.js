9a0b0607afc7ac6786176ac7c30a87328f45705b
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-02 20:45:29 +0300

    upd

--- src/js/cover/markup.js
+++ src/js/cover/markup.js
@@ -1,69 +1,88 @@
-import imgProgect1 from '../../img/covers/projects/project1.png';
-console.log(imgProgect1);
+import imgProject1 from '../../img/covers/projects/project1.png';
+import imgProject1Retina from '../../img/covers/projects/project1@2x.png';
+import imgProject2 from '../../img/covers/projects/project2.png';
+import imgProject2Retina from '../../img/covers/projects/project2@2x.png';
+import imgProject3 from '../../img/covers/projects/project3.png';
+import imgProject3Retina from '../../img/covers/projects/project3@2x.png';
+import imgProject4 from '../../img/covers/projects/project4.png';
+import imgProject4Retina from '../../img/covers/projects/project4@2x.png';
+import imgProject5 from '../../img/covers/projects/project5.png';
+import imgProject5Retina from '../../img/covers/projects/project5@2x.png';
+import imgProject6 from '../../img/covers/projects/project6.png';
+import imgProject6Retina from '../../img/covers/projects/project6@2x.png';
+import imgProject7 from '../../img/covers/projects/project7.png';
+import imgProject7Retina from '../../img/covers/projects/project7@2x.png';
+import imgProject8 from '../../img/covers/projects/project8.png';
+import imgProject8Retina from '../../img/covers/projects/project8@2x.png';
+import imgProject9 from '../../img/covers/projects/project9.png';
+import imgProject9Retina from '../../img/covers/projects/project9@2x.png';
+import imgProject10 from '../../img/covers/projects/project10.png';
+import imgProject10Retina from '../../img/covers/projects/project10@2x.png';
+
 const imagesData = [
   {
-    src: imgProgect1,
-    srcset: './img/covers/projects/project1@2x.png 2x',
+    src: imgProject1,
+    srcset: imgProject1Retina,
     alt: 'Project 1',
   },
   {
-    src: './img/covers/projects/project2.png',
-    srcset: './img/covers/projects/project2@2x.png 2x',
+    src: imgProject2,
+    srcset: imgProject2Retina,
     alt: 'Project 2',
   },
   {
-    src: './img/covers/projects/project3.png',
-    srcset: './img/covers/projects/project3@2x.png 2x',
+    src: imgProject3,
+    srcset: imgProject3Retina,
     alt: 'Project 3',
   },
   {
-    src: './img/covers/projects/project4.png',
-    srcset: './img/covers/projects/project4@2x.png 2x',
+    src: imgProject4,
+    srcset: imgProject4Retina,
     alt: 'Project 4',
   },
   {
-    src: './img/covers/projects/project5.png',
-    srcset: './img/covers/projects/project5@2x.png 2x',
+    src: imgProject5,
+    srcset: imgProject5Retina,
     alt: 'Project 5',
   },
   {
-    src: './img/covers/projects/project6.png',
-    srcset: './img/covers/projects/project6@2x.png 2x',
+    src: imgProject6,
+    srcset: imgProject6Retina,
     alt: 'Project 6',
   },
   {
-    src: './img/covers/projects/project7.png',
-    srcset: './img/covers/projects/project7@2x.png 2x',
+    src: imgProject7,
+    srcset: imgProject7Retina,
     alt: 'Project 7',
   },
   {
-    src: './img/covers/projects/project8.png',
-    srcset: './img/covers/projects/project8@2x.png 2x',
+    src: imgProject8,
+    srcset: imgProject8Retina,
     alt: 'Project 8',
   },
   {
-    src: './img/covers/projects/project9.png',
-    srcset: './img/covers/projects/project9@2x.png 2x',
+    src: imgProject9,
+    srcset: imgProject9Retina,
     alt: 'Project 9',
   },
   {
-    src: './img/covers/projects/project10.png',
-    srcset: './img/covers/projects/project10@2x.png 2x',
+    src: imgProject10,
+    srcset: imgProject10Retina,
     alt: 'Project 10',
   },
   {
-    src: './img/covers/projects/project1.png',
-    srcset: './img/covers/projects/project1@2x.png 2x',
+    src: imgProject1,
+    srcset: imgProject1Retina,
     alt: 'Project 1',
   },
   {
-    src: './img/covers/projects/project2.png',
-    srcset: './img/covers/projects/project2@2x.png 2x',
+    src: imgProject2,
+    srcset: imgProject2Retina,
     alt: 'Project 2',
   },
   {
-    src: './img/covers/projects/project3.png',
-    srcset: './img/covers/projects/project3@2x.png 2x',
+    src: imgProject3,
+    srcset: imgProject3Retina,
     alt: 'Project 3',
   },
 ];
